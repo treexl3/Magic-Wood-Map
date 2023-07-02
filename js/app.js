2c2ef2cdaa95241e84edf8cd8738e532d9b713de
treexl3 <denikquadro@gmail.com>
2023-07-02 12:36:44 +0300

    change

--- js/app.js
+++ js/app.js
@@ -8719,12 +8719,11 @@
         }
         const menuLinks = document.querySelectorAll(".menu__link, .item-control__image-box");
         if (menuLinks.length > 0) {
-            const currentUrl = decodeURIComponent(window.location.href);
-            console.log(currentUrl);
+            const currentUrl = decodeURIComponent(window.location.pathname);
             menuLinks.forEach((menuLink => {
                 if (menuLink.classList.contains("_active")) menuLink.classList.remove("_active");
                 if (menuLink.classList.contains("menu-list_right__link")) return;
-                if (currentUrl.startsWith(menuLink.href)) menuLink.classList.add("_active"); else if (window.location.pathname.includes("/product") || window.location.pathname.includes("/catalog-items")) document.querySelectorAll(".menu__link_catalog").forEach((linkCatalog => linkCatalog.classList.add("_active"))); else if ("/block-page" === window.location.pathname) document.querySelectorAll(".menu__link_blog").forEach((linkCatalog => linkCatalog.classList.add("_active")));
+                if (currentUrl.startsWith(`/Magic-Wood-Map/${menuLink.href}`)) menuLink.classList.add("_active"); else if (window.location.pathname.includes("/product") || window.location.pathname.includes("/catalog-items")) document.querySelectorAll(".menu__link_catalog").forEach((linkCatalog => linkCatalog.classList.add("_active"))); else if ("/block-page" === window.location.pathname) document.querySelectorAll(".menu__link_blog").forEach((linkCatalog => linkCatalog.classList.add("_active")));
             }));
         }
         const values = document.querySelectorAll(".value");
